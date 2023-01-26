dd489ecd4c3d754795e371724e438b0467b71b4b
Brian Vu <emailbrian.vu@gmail.com>
2023-01-26 02:10:38 -0800

    feat: added error handling

--- src/scripts/create-and-update-schedule.sh
+++ src/scripts/create-and-update-schedule.sh
@@ -9,21 +9,27 @@ curl --request GET \
   --url "${URL}" \
   --header "Circle-Token: $CIRCLE_TOKEN" > all_schedules.json
 
-if jq '.' -c all_schedules.json | grep "Project not found"; then
+if jq '.' -c all_schedules.json | grep "Project not found" > /dev/null; then
   echo "The specified project is not found. Please check the project name."
   exit 1
 fi
 
 if jq ".items[] | .name" all_schedules.json | grep "${SCHEDULE_NAME}"; then
-        SCHEDULE_ID=$(jq -r '.items[] | select( .name == '"${SCHEDULE_NAME}"') | .id' all_schedules.json)
-        echo "The ID is ${SCHEDULE_ID}"
-        set -x
-        curl --request PATCH \
-            --url https://circleci.com/api/v2/schedule/"${SCHEDULE_ID}" \
-            --header "Circle-Token: ${CIRCLE_TOKEN}" \
-            --header 'content-type: application/json' \
-            --data "${DATA}"
-        set +x
+  SCHEDULE_ID=$(jq -r '.items[] | select( .name == '"${SCHEDULE_NAME}"') | .id' all_schedules.json)
+  set -x
+  curl --request PATCH \
+      --url https://circleci.com/api/v2/schedule/"${SCHEDULE_ID}" \
+      --header "Circle-Token: ${CIRCLE_TOKEN}" \
+      --header 'content-type: application/json' \
+      --data "${DATA}" > status.json
+  set +x 
+  
+  if jq '.' -c status.json | grep "Invalid input" > /dev/null; then
+    echo -e "\nPlease recheck your json schedule\n"
+    jq '.message' -rc status.json
+    exit 1
+  fi
+
 else
         set -x
         curl --request POST \
