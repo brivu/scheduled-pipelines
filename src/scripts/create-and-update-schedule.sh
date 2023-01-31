0f75cc70355cca2b753113b7cd4886441f55f39d
Brian Vu <emailbrian.vu@gmail.com>
2023-01-31 00:20:49 -0800

    refactor: rewrote conditionals and code

--- src/scripts/create-and-update-schedule.sh
+++ src/scripts/create-and-update-schedule.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ORB_EVAL_SCHEDULE_JSON_PATH=$(eval echo "${ORB_EVAL_SCHEDULE_JSON_PATH}")
-DATA=$(jq '.' -c "${ORB_EVAL_SCHEDULE_JSON_PATH}")
-SCHEDULE_NAME=$(jq '.name' "${ORB_EVAL_SCHEDULE_JSON_PATH}")
+UPDATED_SCHEDULES=$(jq '.schedules' -c "${ORB_EVAL_SCHEDULE_JSON_PATH}")
 
 if echo  "${CIRCLE_BUILD_URL}" | grep -E "GitHub|gh" > /dev/null; then
         VCS="gh"
@@ -15,39 +14,82 @@ URL="https://circleci.com/api/v2/project/${VCS}/${CIRCLE_PROJECT_USERNAME}/${CIR
 
 curl -s --request GET \
   --url "${URL}" \
-  --header "Circle-Token: $CIRCLE_TOKEN" > all_schedules.json
+  --header "Circle-Token: $CIRCLE_TOKEN" > current_schedules.json
 
-jq '.' all_schedules.json
+jq '.' current_schedules.json
 
-if jq '.' -c all_schedules.json | grep "Project not found" > /dev/null; then
+if jq '.' -c current_schedules.json | grep "Project not found" > /dev/null; then
   echo "The specified project is not found. Please check the project name vcs type or namespace."
   exit 1
 fi
 
-if jq ".items[] | .name" all_schedules.json | grep "${SCHEDULE_NAME}"; then
-  SCHEDULE_ID=$(jq -r '.items[] | select( .name == '"${SCHEDULE_NAME}"') | .id' all_schedules.json)
-  set -x
-  curl -s --request PATCH \
-      --url https://circleci.com/api/v2/schedule/"${SCHEDULE_ID}" \
-      --header "Circle-Token: ${CIRCLE_TOKEN}" \
-      --header 'content-type: application/json' \
-      --data "${DATA}" > status.json
-  set +x 
-
-else
-
-  set -x
-  curl -s --request POST \
-      --url "${URL}" \
-      --header "Circle-Token: ${CIRCLE_TOKEN}" \
-      --header 'content-type: application/json' \
-      --data "${DATA}" > status.json
-  set +x
-    if jq '.' -c status.json | grep "Invalid input" > /dev/null; then
-      echo -e "\nPlease recheck your json schedule\n"
-      jq '.message' -rc status.json
-      exit 1
+# Delete schedules 
+jq -cr '.items[] | .name' current_schedules.json | while read -r current_schedule_names; 
+do
+    if echo "$UPDATED_SCHEDULES" |  grep -v  "${current_schedule_names}" > /dev/null; then
+        SCHEDULE_ID=$(jq -r '.items[] | select( .name == '\""${current_schedule_names}"\"') | .id' all.json)
+            set -x
+            curl -s --request DELETE \
+            --url https://circleci.com/api/v2/schedule/"${SCHEDULE_ID}" \
+            --header "Circle-Token: ${CIRCLE_TOKEN}" > status.json
+            set +x
     fi
-fi
+done
+
+
+CURRENT_SCHEDULES=$(jq -cr '.items[]'  current_schedules.json)
+jq -c '.schedules[]' "${ORB_EVAL_SCHEDULE_JSON_PATH}" | while read -r new_schedule;
+do
+    new_schedule_name=$(echo "${new_schedule}" | jq -cr '.name')
+    if echo "${CURRENT_SCHEDULES}" | grep "${new_schedule_name}" >/dev/null; then
+        SCHEDULE_ID=$(jq -r '.items[] | select( .name == '\""${new_schedule_name}\""') | .id' all.json)
+        set -x
+        curl -s --request PATCH \
+        --url https://circleci.com/api/v2/schedule/"${SCHEDULE_ID}" \
+        --header "Circle-Token: ${CIRCLE_TOKEN}" \
+        --header 'content-type: application/json' \
+        --data "${new_schedule}" > status.json
+        set +x 
+    else
+        set -x
+          curl -s --request POST \
+              --url "${URL}" \
+              --header "Circle-Token: ${CIRCLE_TOKEN}" \
+              --header 'content-type: application/json' \
+              --data "${new_schedule}" > status.json
+          set +x
+            if jq '.' -c status.json | grep "Invalid input" > /dev/null; then
+              echo -e "\nPlease recheck your json schedule\n"
+              jq '.message' -rc status.json
+              exit 1
+            fi
+      fi
+done
+
+# if jq ".items[] | .name" current_schedules.json | grep "${SCHEDULE_NAME}"; then
+#   SCHEDULE_ID=$(jq -r '.items[] | select( .name == '"${SCHEDULE_NAME}"') | .id' current_schedules.json)
+#   set -x
+#   curl -s --request PATCH \
+#       --url https://circleci.com/api/v2/schedule/"${SCHEDULE_ID}" \
+#       --header "Circle-Token: ${CIRCLE_TOKEN}" \
+#       --header 'content-type: application/json' \
+#       --data "${DATA}" > status.json
+#   set +x 
+
+# else
+
+#   set -x
+#   curl -s --request POST \
+#       --url "${URL}" \
+#       --header "Circle-Token: ${CIRCLE_TOKEN}" \
+#       --header 'content-type: application/json' \
+#       --data "${DATA}" > status.json
+#   set +x
+#     if jq '.' -c status.json | grep "Invalid input" > /dev/null; then
+#       echo -e "\nPlease recheck your json schedule\n"
+#       jq '.message' -rc status.json
+#       exit 1
+#     fi
+# fi
 
 jq '.' status.json
--- src/scripts/delete-schedule.sh
+++ src/scripts/delete-schedule.sh
@@ -6,15 +6,18 @@ URL="https://circleci.com/api/v2/project/${ORB_VAL_VCS_TYPE}/${ORB_VAL_NAMESPACE
 
 curl -s --request GET \
   --url "${URL}" \
-  --header "Circle-Token: $CIRCLE_TOKEN" > all_schedules.json
+  --header "Circle-Token: $CIRCLE_TOKEN" > existing_schedules.json
 
-if jq '.' -c all_schedules.json | grep "Project not found" > /dev/null; then
+if jq '.' -c existing_schedules.json | grep "Project not found" > /dev/null; then
   echo "The specified project is not found. Please check the project name vcs type or namespace."
   exit 1
 fi
 
-if jq ".items[] | .name" all_schedules.json | grep "${ORB_EVAL_SCHEDULE_NAME}" > /dev/null; then
-  SCHEDULE_ID=$(jq -r '.items[] | select( .name == '"\"${ORB_EVAL_SCHEDULE_NAME}\""') | .id' all_schedules.json)
+
+
+
+if jq ".items[] | .name" existing_schedules.json | grep "${ORB_EVAL_SCHEDULE_NAME}" > /dev/null; then
+  SCHEDULE_ID=$(jq -r '.items[] | select( .name == '"\"${ORB_EVAL_SCHEDULE_NAME}\""') | .id' existing_schedules.json)
   
   set -x
   curl -s --request DELETE \
