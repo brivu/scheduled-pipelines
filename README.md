c26dad933c35d45ecb5164c694cb450169fdf1d1
Brian Vu <emailbrian.vu@gmail.com>
2023-01-31 11:24:11 -0800

    chore: added delete command to config

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# Orb Template
+# CircleCI Scheduled Pipelines Orb
 
 
 [![CircleCI Build Status](https://circleci.com/gh/brivu/scheduled-piplines.svg?style=shield "CircleCI Build Status")](https://circleci.com/gh/brivu/scheduled-piplines) [![CircleCI Orb Version](https://badges.circleci.com/orbs/brivu/scheduled-piplines.svg)](https://circleci.com/orbs/registry/orb/brivu/scheduled-piplines) [![GitHub License](https://img.shields.io/badge/license-MIT-lightgrey.svg)](https://raw.githubusercontent.com/brivu/scheduled-piplines/master/LICENSE) [![CircleCI Community](https://img.shields.io/badge/community-CircleCI%20Discuss-343434.svg)](https://discuss.circleci.com/c/ecosystem/orbs)
 
-
-
-A project template for Orbs.
-
-This repository is designed to be automatically ingested and modified by the CircleCI CLI's `orb init` command.
+A CircleCI orb for updating pipeline schedules.
 
 _**Edit this area to include a custom title and description.**_
 
@@ -23,17 +19,3 @@ _**Edit this area to include a custom title and description.**_
 
 We welcome [issues](https://github.com/brivu/scheduled-piplines/issues) to and [pull requests](https://github.com/brivu/scheduled-piplines/pulls) against this repository!
 
-### How to Publish An Update
-1. Merge pull requests with desired changes to the main branch.
-    - For the best experience, squash-and-merge and use [Conventional Commit Messages](https://conventionalcommits.org/).
-2. Find the current version of the orb.
-    - You can run `circleci orb info brivu/scheduled-piplines | grep "Latest"` to see the current version.
-3. Create a [new Release](https://github.com/brivu/scheduled-piplines/releases/new) on GitHub.
-    - Click "Choose a tag" and _create_ a new [semantically versioned](http://semver.org/) tag. (ex: v1.0.0)
-      - We will have an opportunity to change this before we publish if needed after the next step.
-4.  Click _"+ Auto-generate release notes"_.
-    - This will create a summary of all of the merged pull requests since the previous release.
-    - If you have used _[Conventional Commit Messages](https://conventionalcommits.org/)_ it will be easy to determine what types of changes were made, allowing you to ensure the correct version tag is being published.
-5. Now ensure the version tag selected is semantically accurate based on the changes included.
-6. Click _"Publish Release"_.
-    - This will push a new tag and trigger your publishing pipeline on CircleCI.
--- src/scripts/update-schedules.sh
+++ src/scripts/update-schedules.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ORB_EVAL_SCHEDULE_JSON_PATH=$(eval echo "${ORB_EVAL_SCHEDULE_JSON_PATH}")
 UPDATED_SCHEDULES=$(jq '.schedules' -c "${ORB_EVAL_SCHEDULE_JSON_PATH}")
-
+SCHEDULE_DATA=$(mktmp -d)
 if echo  "${CIRCLE_BUILD_URL}" | grep -E "GitHub|gh" > /dev/null; then
         VCS="gh"
-elif echo  "${CIRCLE_BUILD_URL}" | grep -E "BitBucke|bb" > /dev/null; then
+elif echo  "${CIRCLE_BUILD_URL}" | grep -E "BitBucket|bb" > /dev/null; then
         VCS="bb"
 else
         VCS="circleci"
@@ -14,20 +14,20 @@ URL="https://circleci.com/api/v2/project/${VCS}/${CIRCLE_PROJECT_USERNAME}/${CIR
 
 curl -s --request GET \
   --url "${URL}" \
-  --header "Circle-Token: $CIRCLE_TOKEN" > current_schedules.json
+  --header "Circle-Token: $CIRCLE_TOKEN" > "${SCHEDULE_DATA}/current_schedules.json"
 
-jq '.' current_schedules.json
+jq '.' "${SCHEDULE_DATA}/current_schedules.json"
 
-if jq '.' -c current_schedules.json | grep "Project not found" > /dev/null; then
+if jq '.' -c "${SCHEDULE_DATA}/current_schedules.json" | grep "Project not found" > /dev/null; then
   echo "The specified project is not found. Please check the project name vcs type or namespace."
   exit 1
 fi
 
 # Delete schedules 
-jq -cr '.items[] | .name' current_schedules.json | while read -r current_schedule_names; 
+jq -cr '.items[] | .name' "${SCHEDULE_DATA}/current_schedules.json" | while read -r current_schedule_names; 
 do
     if echo "$UPDATED_SCHEDULES" |  grep -v  "${current_schedule_names}" > /dev/null; then
-        SCHEDULE_ID=$(jq -r '.items[] | select( .name == '\""${current_schedule_names}"\"') | .id' current_schedules.json)
+        SCHEDULE_ID=$(jq -r '.items[] | select( .name == '\""${current_schedule_names}"\"') | .id' "${SCHEDULE_DATA}/current_schedules.json")
             set -x
             curl -s --request DELETE \
             --url https://circleci.com/api/v2/schedule/"${SCHEDULE_ID}" \
@@ -37,12 +37,12 @@ do
 done
 
 
-CURRENT_SCHEDULES=$(jq -cr '.items[]'  current_schedules.json)
+CURRENT_SCHEDULES=$(jq -cr '.items[]'  "${SCHEDULE_DATA}/current_schedules.json")
 jq -c '.schedules[]' "${ORB_EVAL_SCHEDULE_JSON_PATH}" | while read -r new_schedule;
 do
     new_schedule_name=$(echo "${new_schedule}" | jq -cr '.name')
     if echo "${CURRENT_SCHEDULES}" | grep "${new_schedule_name}" >/dev/null; then
-        SCHEDULE_ID=$(jq -r '.items[] | select( .name == '\""${new_schedule_name}\""') | .id' current_schedules.json)
+        SCHEDULE_ID=$(jq -r '.items[] | select( .name == '\""${new_schedule_name}\""') | .id' "${SCHEDULE_DATA}/current_schedules.json")
         set -x
         curl -s --request PATCH \
         --url https://circleci.com/api/v2/schedule/"${SCHEDULE_ID}" \
@@ -68,6 +68,6 @@ done
 
 curl -s --request GET \
   --url "${URL}" \
-  --header "Circle-Token: $CIRCLE_TOKEN" > current_schedules.json
+  --header "Circle-Token: $CIRCLE_TOKEN" > "${SCHEDULE_DATA}/current_schedules.json"
 
-jq '.' current_schedules.json
+jq '.' "${SCHEDULE_DATA}/current_schedules.json"
